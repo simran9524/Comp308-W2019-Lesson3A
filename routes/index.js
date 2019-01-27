c9cccdc9e98405c89a88416afdd211994e075a08
simran9524 <simrankalra9524@gmail.com>
2019-01-26 20:40:18 -0500

    Initial Commit

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+let express = require('express');
+let router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Home' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('index', { title: 'About' });
+});
+
+router.get('/contact', (req, res, next) => {
+  res.render('index', { title: 'Contact' });
+});
+
+router.get('/products', (req, res, next) => {
+  res.render('index', { title: 'Products' });
+});
+
+router.get('/services', (req, res, next) => {
+  res.render('index', { title: 'Services' });
+});
+
+module.exports = router;
